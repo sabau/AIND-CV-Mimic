0f290d43fff187595e818a0786b706336bd05acf
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2018-01-27 22:16:39 +0100

    shall we implement multiplayer challenges?

--- mimic.js
+++ mimic.js
@@ -24,117 +24,117 @@ var emojis = [ 128528, 9786, 128515, 128524, 128527, 128521, 128535, 128539, 128
 
 // Update target emoji being displayed by supplying a unicode value
 function setTargetEmoji(code) {
-  $("#target").html("&#" + code + ";");
+    $("#target").html("&#" + code + ";");
 }
 
 // Convert a special character to its unicode value (can be 1 or 2 units long)
 function toUnicode(c) {
-  if(c.length == 1)
-    return c.charCodeAt(0);
-  return ((((c.charCodeAt(0) - 0xD800) * 0x400) + (c.charCodeAt(1) - 0xDC00) + 0x10000));
+    if(c.length == 1)
+        return c.charCodeAt(0);
+    return ((((c.charCodeAt(0) - 0xD800) * 0x400) + (c.charCodeAt(1) - 0xDC00) + 0x10000));
 }
 
 // Update score being displayed
 function setScore(correct, total) {
-  $("#score").html("Score: " + correct + " / " + total);
+    $("#score").html("Score: " + correct + " / " + total);
 }
 
 // Display log messages and tracking results
 function log(node_name, msg) {
-  $(node_name).append("<span>" + msg + "</span><br />")
+    $(node_name).append("<span>" + msg + "</span><br />")
 }
 
 // --- Callback functions ---
 
 // Start button
 function onStart() {
-  if (detector && !detector.isRunning) {
-    $("#logs").html("");  // clear out previous log
-    detector.start();  // start detector
-  }
-  log('#logs', "Start button pressed");
+    if (detector && !detector.isRunning) {
+        $("#logs").html("");  // clear out previous log
+        detector.start();  // start detector
+    }
+    log('#logs', "Start button pressed");
 }
 
 // Stop button
 function onStop() {
-  log('#logs', "Stop button pressed");
-  if (detector && detector.isRunning) {
-    detector.removeEventListener();
-    detector.stop();  // stop detector
-  }
+    log('#logs', "Stop button pressed");
+    if (detector && detector.isRunning) {
+        detector.removeEventListener();
+        detector.stop();  // stop detector
+    }
 };
 
 // Reset button
 function onReset() {
-  log('#logs', "Reset button pressed");
-  if (detector && detector.isRunning) {
-    detector.reset();
-  }
-  $('#results').html("");  // clear out results
-  $("#logs").html("");  // clear out previous log
+    log('#logs', "Reset button pressed");
+    if (detector && detector.isRunning) {
+        detector.reset();
+    }
+    $('#results').html("");  // clear out results
+    $("#logs").html("");  // clear out previous log
 
-  // TODO(optional): You can restart the game as well
-  // <your code here>
+    // TODO(optional): You can restart the game as well
+    // <your code here>
 };
 
 // Add a callback to notify when camera access is allowed
 detector.addEventListener("onWebcamConnectSuccess", function() {
-  log('#logs', "Webcam access allowed");
+    log('#logs', "Webcam access allowed");
 });
 
 // Add a callback to notify when camera access is denied
 detector.addEventListener("onWebcamConnectFailure", function() {
-  log('#logs', "webcam denied");
-  console.log("Webcam access denied");
+    log('#logs', "webcam denied");
+    console.log("Webcam access denied");
 });
 
 // Add a callback to notify when detector is stopped
 detector.addEventListener("onStopSuccess", function() {
-  log('#logs', "The detector reports stopped");
-  $("#results").html("");
+    log('#logs', "The detector reports stopped");
+    $("#results").html("");
 });
 
 // Add a callback to notify when the detector is initialized and ready for running
 detector.addEventListener("onInitializeSuccess", function() {
-  log('#logs', "The detector reports initialized");
-  //Display canvas instead of video feed because we want to draw the feature points on it
-  $("#face_video_canvas").css("display", "block");
-  $("#face_video").css("display", "none");
+    log('#logs', "The detector reports initialized");
+    //Display canvas instead of video feed because we want to draw the feature points on it
+    $("#face_video_canvas").css("display", "block");
+    $("#face_video").css("display", "none");
 
-  // TODO(optional): Call a function to initialize the game, if needed
-  // <your code here>
+    // TODO(optional): Call a function to initialize the game, if needed
+    // <your code here>
 });
 
 // Add a callback to receive the results from processing an image
 // NOTE: The faces object contains a list of the faces detected in the image,
 //   probabilities for different expressions, emotions and appearance metrics
 detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
-  var canvas = $('#face_video_canvas')[0];
-  if (!canvas)
-    return;
+    var canvas = $('#face_video_canvas')[0];
+    if (!canvas)
+        return;
 
-  // Report how many faces were found
-  $('#results').html("");
-  log('#results', "Timestamp: " + timestamp.toFixed(2));
-  log('#results', "Number of faces found: " + faces.length);
-  if (faces.length > 0) {
-    // Report desired metrics
-    log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
-    log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
-      return val.toFixed ? Number(val.toFixed(0)) : val;
-    }));
-    log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
-      return val.toFixed ? Number(val.toFixed(0)) : val;
-    }));
-    log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
+    // Report how many faces were found
+    $('#results').html("");
+    log('#results', "Timestamp: " + timestamp.toFixed(2));
+    log('#results', "Number of faces found: " + faces.length);
+    if (faces.length > 0) {
+        // Report desired metrics
+        log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
+        log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
+                return val.toFixed ? Number(val.toFixed(0)) : val;
+            }));
+        log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
+                return val.toFixed ? Number(val.toFixed(0)) : val;
+            }));
+        log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
 
-    // Call functions to draw feature points and dominant emoji (for the first face only)
-    drawFeaturePoints(canvas, image, faces[0]);
-    drawEmoji(canvas, image, faces[0]);
+        // Call functions to draw feature points and dominant emoji (for the first face only)
+        var corner = drawFeaturePoints(canvas, image, faces[0]);
+        drawEmoji(canvas, image, faces[0], corner);
 
-    // TODO: Call your function to run the game (define it first!)
-    // <your code here>
-  }
+        // TODO: Call your function to run the game (define it first!)
+        // <your code here>
+    }
 });
 
 
@@ -142,35 +142,31 @@ detector.addEventListener("onImageResultsSuccess", function(faces, image, timest
 
 // Draw the detected facial feature points on the image
 function drawFeaturePoints(canvas, img, face) {
-  // Obtain a 2D context object to draw on the canvas
-  var ctx = canvas.getContext('2d');
-
-  // TODO: Set the stroke and/or fill style you want for each feature point marker
-  // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D#Fill_and_stroke_styles
-  // <your code here>
-  
-  // Loop over each feature point in the face
-  for (var id in face.featurePoints) {
-    var featurePoint = face.featurePoints[id];
-
-    // TODO: Draw feature point, e.g. as a circle using ctx.arc()
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-    // <your code here>
-  }
+    // Obtain a 2D context object to draw on the canvas
+    var ctx = canvas.getContext('2d');
+    ctx.strokeStyle = 'grey';
+    ctx.fillStyle = 'white';
+    const radius = 5;
+    var corner = {x: 0, y: 100000};
+    // Loop over each feature point in the face
+    for (var faceId in face.featurePoints) {
+        var featurePoint = face.featurePoints[faceId];
+        ctx.beginPath();
+        ctx.arc(featurePoint.x, featurePoint.y, radius, 0, Math.PI * 2);
+        ctx.stroke();
+        if (featurePoint.x > corner.x) corner.x = featurePoint.x;
+        if (featurePoint.y < corner.y) corner.y = featurePoint.y;
+    }
+    return corner;
 }
 
 // Draw the dominant emoji on the image
-function drawEmoji(canvas, img, face) {
-  // Obtain a 2D context object to draw on the canvas
-  var ctx = canvas.getContext('2d');
+function drawEmoji(canvas, img, face, corner) {
+    // Obtain a 2D context object to draw on the canvas
+    var ctx = canvas.getContext('2d');
+    ctx.font = '48px serif';
+    ctx.fillText(face.emojis.dominantEmoji, corner.x, corner.y);
 
-  // TODO: Set the font and style you want for the emoji
-  // <your code here>
-  
-  // TODO: Draw it using ctx.strokeText() or fillText()
-  // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
-  // TIP: Pick a particular feature point as an anchor so that the emoji sticks to your face
-  // <your code here>
 }
 
 // TODO: Define any variables and functions to implement the Mimic Me! game mechanics
